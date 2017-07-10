16015235c54d60716f418962036f08c0b9dee626
blackburnman04 <brendan.rodgers@gmail.com>
2017-07-10 21:43:57 +0100

    changed pagination method to scrape
    changed from limited range to paginated method, to allow all pages to be scraped instead of just the previous range

--- scraper.py
+++ scraper.py
@@ -5,7 +5,7 @@ import sys
 from bs4 import BeautifulSoup
 
 houses = []
-
+# first page doesnt use page- string in url or had to do this separately for first page and add results to the same list that will contain the paginated results
 url = "https://www.propertypal.com/property-to-rent/newtownabbey/"
 page=requests.get(url)
 soup=BeautifulSoup(page.text,"lxml")
@@ -31,35 +31,39 @@ for item in g_data:
     houses.append(course)
         
 
-for i in range(1,15)#page range pages 1 to 15 are crawled:
+button_next = soup.find("a", {"class": "btn paging-next"}, href=True) # used for looping through pages pagination. Better than previous range solution which was limited to certain page numbers
+while button_next: # while loop for pagination
     time.sleep(2)#delay time requests are sent so we don't get kicked by server
-    url2 = "https://www.propertypal.com/property-to-rent/newtownabbey/page-{0}".format(i)
+    url2 = "https://www.propertypal.com{0}".format(button_next["href"]) # added page number string to link e.g /page-3
     page2=requests.get(url2)
+    print(url2)
     soup=BeautifulSoup(page2.text,"lxml")
     g_data = soup.findAll("div", {"class": "propbox-details"})
     for item in g_data:
         try:
-            title = item.find_all("span", {"class": "propbox-addr"})[0].text
+            title = item.find_all("span", {"class": "propbox-addr"})[0].text # scrape address
         except:
             pass
         try:
-            town = item.find_all("span", {"class": "propbox-town"})[0].text
+            town = item.find_all("span", {"class": "propbox-town"})[0].text # scrape town
         except:
             pass
         try:
-            price = item.find_all("span", {"class": "price-value"})[0].text
+            price = item.find_all("span", {"class": "price-value"})[0].text # scrape price
         except:
             pass
         try:
-            period = item.find_all("span", {"class": "price-period"})[0].text
+            period = item.find_all("span", {"class": "price-period"})[0].text # scrape period of payment e.g weekly/monthly
         except:
             pass
         
-        course=[title,town,price,period]
-        houses.append(course)
+        course=[title,town,price,period] # puts scraped values in list
+        houses.append(course) 
 
-# output to csv file        
-with open ('newtownabbeyrentalproperties.csv','w') as file:
+        button_next = soup.find("a", {"class" : "btn paging-next"}, href=True)
+
+        
+with open ('newtownabbeyrentalproperties.csv','w') as file: # output scraped data to csv file
    writer=csv.writer(file)
    writer.writerow(['Address','Town', 'Price', 'Period'])
    for row in houses:
