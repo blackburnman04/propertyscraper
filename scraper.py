f732e77a462c2235190fdbaad97f2ef30ab12123
blackburnman04 <brendan.rodgers@gmail.com>
2017-07-12 23:34:26 +0100

    Added Command Line menu + search properties to buy
    I added a command menu which the user can enter number 1, 2 or 3, 1 being search for rental properties, 2 being search for properties to buy, and 3 being exit the script. 
    
    Once option 1 or 2 are selected, the user will be prompted to enter the area they which to search for properties to buy or rent, e,g. Newtownabbey.
    
    Scraping will commence. Once scraping is complete, the command line will prompt user to enter filename they which to save, e.g. newtownabbeyrentals
    
    Once this is entered an .csv file will be created with the filename entered by the user, in the same directory as this script. This file will contain the scraped data.

--- scraper.py
+++ scraper.py
@@ -5,70 +5,155 @@ import sys
 from bs4 import BeautifulSoup
 
 houses = []
-# first page doesnt use page- string in url or had to do this separately for first page and add results to the same list that will contain the paginated results
-url = "https://www.propertypal.com/property-to-rent/newtownabbey/"
-page=requests.get(url)
-soup=BeautifulSoup(page.text,"lxml")
-g_data = soup.findAll("div", {"class": "propbox-details"})
-for item in g_data:
-    try:
-        title = item.find_all("span", {"class": "propbox-addr"})[0].text
-    except:
-        pass
-    try:
-        town = item.find_all("span", {"class": "propbox-town"})[0].text
-    except:
-        pass
-    try:
-        price = item.find_all("span", {"class": "price-value"})[0].text
-    except:
-        pass
-    try:
-        period = item.find_all("span", {"class": "price-period"})[0].text
-    except:
-        pass
-    course=[title,town,price,period]
-    houses.append(course)
-        
 
-button_next = soup.find("a", {"class": "btn paging-next"}, href=True) # used for looping through pages pagination. Better than previous range solution which was limited to certain page numbers
-while button_next: # while loop for pagination
-    time.sleep(2)#delay time requests are sent so we don't get kicked by server
-    url2 = "https://www.propertypal.com{0}".format(button_next["href"]) # added page number string to link e.g /page-3
-    page2=requests.get(url2)
-    print(url2)
-    soup=BeautifulSoup(page2.text,"lxml")
+
+
+def save():
+    filename = input("Please input name of file to be saved")        
+    with open (filename + '.csv','w') as file:
+       writer=csv.writer(file)
+       writer.writerow(['Address','Town', 'Price', 'Period'])
+       for row in houses:
+          writer.writerow(row)
+    print("File Saved Successfully")
+
+
+def rent():
+    
+    search = input("Please enter town")
+
+    url = "https://www.propertypal.com/property-to-rent/" + search
+    page=requests.get(url)
+    soup=BeautifulSoup(page.text,"lxml")
     g_data = soup.findAll("div", {"class": "propbox-details"})
     for item in g_data:
         try:
-            title = item.find_all("span", {"class": "propbox-addr"})[0].text # scrape address
+            title = item.find_all("span", {"class": "propbox-addr"})[0].text
         except:
             pass
         try:
-            town = item.find_all("span", {"class": "propbox-town"})[0].text # scrape town
+            town = item.find_all("span", {"class": "propbox-town"})[0].text
         except:
             pass
         try:
-            price = item.find_all("span", {"class": "price-value"})[0].text # scrape price
+            price = item.find_all("span", {"class": "price-value"})[0].text
         except:
             pass
         try:
-            period = item.find_all("span", {"class": "price-period"})[0].text # scrape period of payment e.g weekly/monthly
+            period = item.find_all("span", {"class": "price-period"})[0].text
         except:
             pass
-        
-        course=[title,town,price,period] # puts scraped values in list
-        houses.append(course) 
+        course=[title,town,price,period]
+        houses.append(course)
 
-        button_next = soup.find("a", {"class" : "btn paging-next"}, href=True)
 
-        
-with open ('newtownabbeyrentalproperties.csv','w') as file: # output scraped data to csv file
-   writer=csv.writer(file)
-   writer.writerow(['Address','Town', 'Price', 'Period'])
-   for row in houses:
-      writer.writerow(row)
+    button_next = soup.find("a", {"class": "btn paging-next"}, href=True)
+    while button_next:
+        time.sleep(2)#delay time requests are sent so we don't get kicked by server
+        url = "https://www.propertypal.com{0}".format(button_next["href"])
+        page=requests.get(url)
+        print(url + "  scraped successfully")
+        soup=BeautifulSoup(page.text,"lxml")
+        g_data = soup.findAll("div", {"class": "propbox-details"})
+        for item in g_data:
+            try:
+                title = item.find_all("span", {"class": "propbox-addr"})[0].text
+            except:
+                pass
+            try:
+                town = item.find_all("span", {"class": "propbox-town"})[0].text
+            except:
+                pass
+            try:
+                price = item.find_all("span", {"class": "price-value"})[0].text
+            except:
+                pass
+            try:
+                period = item.find_all("span", {"class": "price-period"})[0].text
+            except:
+                pass
+            button_next = soup.find("a", {"class" : "btn paging-next"}, href=True)
 
-        
-        
-        
+            course=[title,town,price,period]
+            houses.append(course)
+
+
+
+def buy():
+    
+    search = input("Please enter town")
+
+    url = "https://www.propertypal.com/property-for-sale/" + search
+    page=requests.get(url)
+    soup=BeautifulSoup(page.text,"lxml")
+    g_data = soup.findAll("div", {"class": "propbox-details"})
+    for item in g_data:
+        try:
+            title = item.find_all("span", {"class": "propbox-addr"})[0].text
+        except:
+            pass
+        try:
+            town = item.find_all("span", {"class": "propbox-town"})[0].text
+        except:
+            pass
+        try:
+            price = item.find_all("span", {"class": "price-value"})[0].text
+        except:
+            pass
+        try:
+            period = item.find_all("span", {"class": "price-period"})[0].text
+        except:
+            pass
+        course=[title,town,price,period]
+        houses.append(course)
+
+
+    button_next = soup.find("a", {"class": "btn paging-next"}, href=True)
+    while button_next:
+        time.sleep(2)#delay time requests are sent so we don't get kicked by server
+        url = "https://www.propertypal.com{0}".format(button_next["href"])
+        page=requests.get(url)
+        print(url + "  scraped successfully")
+        soup=BeautifulSoup(page.text,"lxml")
+        g_data = soup.findAll("div", {"class": "propbox-details"})
+        for item in g_data:
+            try:
+                title = item.find_all("span", {"class": "propbox-addr"})[0].text
+            except:
+                pass
+            try:
+                town = item.find_all("span", {"class": "propbox-town"})[0].text
+            except:
+                pass
+            try:
+                price = item.find_all("span", {"class": "price-value"})[0].text
+            except:
+                pass
+            try:
+                period = item.find_all("span", {"class": "price-period"})[0].text
+            except:
+                pass
+            button_next = soup.find("a", {"class" : "btn paging-next"}, href=True)
+
+            course=[title,town,price,period]
+            houses.append(course)
+
+
+def menu():
+        strs = ('Enter 1 to search Rental Properties\n'
+                'Enter 2 to search for Properties to Buy\n'
+                'Enter 3 to Exit\n' )
+        choice = input(strs)
+        return int(choice) 
+
+while True:          #use while True
+    choice = menu()
+    if choice == 1:
+        rent()
+        save()
+    elif choice == 2:
+        buy()
+        save()
+    elif choice == 3:
+        break
+    
