af85eaa5a43c5473d4ed5e8407811809c4a34d4a
blackburnman04 <brendan.rodgers@gmail.com>
2017-07-16 13:47:46 +0100

    Fixed error when scraping properties to buy
    Error was that as properties for sale do not have period of payment, e.g. per month, the scraper was having a 'period' not defined error, to correct this I had to add a new function for the properties to buy that didn't scrape the period section of the site, so the error would not occur.

--- scraper.py
+++ scraper.py
@@ -6,13 +6,11 @@ from bs4 import BeautifulSoup
 
 houses = []
 
-
 def deletelist():
     houses.clear()
 
 
-
-def saverent():
+def savefile():
     filename = input("Please input name of file to be saved")        
     with open (filename + '.csv','w') as file:
        writer=csv.writer(file)
@@ -21,28 +19,15 @@ def saverent():
           writer.writerow(row)
     print("File Saved Successfully")
 
-def savebuy():
-    filename = input("Please input name of file to be saved")        
-    with open (filename + '.csv','w') as file:
-       writer=csv.writer(file)
-       writer.writerow(['Address','Town', 'Price', 'Period'])
-       for row in houses:
-          writer.writerow(row)
-    print("File Saved Successfully")
 
 def appendhouses(scrape):
     houses.append(scrape)
-
-    
 
 def makesoup(url):
     page=requests.get(url)
     print(url + "  scraped successfully")
     return BeautifulSoup(page.text,"lxml")
 
-def rentallist(scrape):
-    houses.insert(1, scrape)
-    
 def rentalscrape(g_data):
     for item in g_data:
         try:
@@ -54,6 +39,10 @@ def rentalscrape(g_data):
         except:
             pass
         try:
+            description = item.find_all("p", {"class": "propbox-brief"})[0].text
+        except:
+            pass
+        try:
             price = item.find_all("span", {"class": "price-value"})[0].text
         except:
             pass
@@ -61,10 +50,28 @@ def rentalscrape(g_data):
             period = item.find_all("span", {"class": "price-period"})[0].text
         except:
             pass
-        appendhouses(scrape=[title,town,price,period])
-        
-    
+        appendhouses(scrape=[title,town,description,price,period])
 
+def buyscrape(g_data):
+    for item in g_data:
+        try:
+            title = item.find_all("span", {"class": "propbox-addr"})[0].text
+        except:
+            pass
+        try:
+            town = item.find_all("span", {"class": "propbox-town"})[0].text
+        except:
+            pass
+        try:
+            description = item.find_all("p", {"class": "propbox-brief"})[0].text
+        except:
+            pass
+        try:
+            price = item.find_all("span", {"class": "price-value"})[0].text
+        except:
+            pass
+        appendhouses(scrape=[title,town,description,price])
+        
 def rent():
     
     search = input("Please enter town")
@@ -79,23 +86,19 @@ def rent():
 
         button_next = soup.find("a", {"class" : "btn paging-next"}, href=True)
 
-
 def buy():
     
     search = input("Please enter town")
     soup=makesoup(url = "https://www.propertypal.com/property-for-sale/" + search)
-    rentalscrape(g_data = soup.findAll("div", {"class": "propbox-details"}))
+    buyscrape(g_data = soup.findAll("div", {"class": "propbox-details"}))
 
     button_next = soup.find("a", {"class": "btn paging-next"}, href=True)
     while button_next:
         time.sleep(2)#delay time requests are sent so we don't get kicked by server
         soup=makesoup(url = "https://www.propertypal.com{0}".format(button_next["href"]))
-        rentalscrape(g_data = soup.findAll("div", {"class": "propbox-details"}))
+        buyscrape(g_data = soup.findAll("div", {"class": "propbox-details"}))
 
         button_next = soup.find("a", {"class" : "btn paging-next"}, href=True)
-
-            
-
 
 def menu():
         strs = ('Enter 1 to search Rental Properties\n'
@@ -108,13 +111,11 @@ while True:          #use while True
     choice = menu()
     if choice == 1:
         rent()
-        saverent()
+        savefile()
         deletelist()
     elif choice == 2:
         buy()
-        savebuy()
+        savefile()
         deletelist()
     elif choice == 3:
         break
-    
-
